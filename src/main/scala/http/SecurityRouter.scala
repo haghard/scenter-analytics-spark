593a9e1e7a87a87b0842d1cf0122ab9654eeadfe
Vadim Bondarev <haghard84@gmail.com>
2016-02-20 14:47:42 +0300

    experement

--- src/main/scala/http/SecurityRouter.scala
+++ src/main/scala/http/SecurityRouter.scala
@@ -129,10 +129,14 @@ trait SecurityRouter extends DefaultRestMicroservice with Directives { mixin: Mi
   private def twitterR(implicit ec: ExecutionContext): Route =
     path("login-twitter") {
       get {
-        val service = twitter.oAuthService.callback(s"http://$domain:$httpPort/$pathPrefix/twitter-sign-in").build()
-        val requestToken = service.getRequestToken
-        val url = service.getAuthorizationUrl(requestToken)
-        redirect(akka.http.scaladsl.model.Uri(url), StatusCodes.PermanentRedirect)
+        extractHost { host =>
+          //domain httpPort
+          //val service = twitter.oAuthService.callback(s"http://$domain:$httpPort/$pathPrefix/twitter-sign-in").build()
+          val service = twitter.oAuthService.callback(s"http://host:9000/twitter-sign-in").build()
+          val requestToken = service.getRequestToken
+          val url = service.getAuthorizationUrl(requestToken)
+          redirect(akka.http.scaladsl.model.Uri(url), StatusCodes.PermanentRedirect)
+        }
       }
     } ~ path("twitter-sign-in") {
       get {
