1fb66549845c20cafab35e73a405b0801d383cb5
Vadim Bondarev <haghard84@gmail.com>
2016-02-22 12:57:01 +0300

    experement

--- src/main/scala/http/SecurityRouter.scala
+++ src/main/scala/http/SecurityRouter.scala
@@ -149,7 +149,7 @@ trait SecurityRouter extends DefaultRestMicroservice with Directives { mixin: Mi
     path("login-twitter") {
       get {
         extractHost { host =>
-          val service = twitter.oAuthService.callback(s"http://$domain:$httpPort/$pathPrefix/twitter-sign-in").build()
+          val service = twitter.oAuthService.callback(s"http://$domain:$httpPort/twitter-sign-in").build()
           val requestToken = service.getRequestToken
           val url = service.getAuthorizationUrl(requestToken)
           system.log.info(s"login-twitter: $host")
