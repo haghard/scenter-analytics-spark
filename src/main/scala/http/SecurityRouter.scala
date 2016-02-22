633feb2a98d229328a796860f412d08ac587fbce
Vadim Bondarev <haghard84@gmail.com>
2016-02-22 18:32:13 +0300

    fix routes

--- src/main/scala/http/SecurityRouter.scala
+++ src/main/scala/http/SecurityRouter.scala
@@ -65,7 +65,7 @@ trait SecurityRouter extends DefaultRestMicroservice with Directives { mixin: Mi
         extractHost { host =>
           system.log.info(s"frontend-login-github from: $host")
           //FIXME port 9000 put address in the params
-          val service = twitter.oAuthService.callback(s"http://$host:9000/github-callback").build()
+          val service = github.oAuthService.callback(s"http://$host:9000/github-callback").build()
           val requestToken = service.getRequestToken
           val url = service.getAuthorizationUrl(requestToken)
           redirect(akka.http.scaladsl.model.Uri(url), StatusCodes.PermanentRedirect)
