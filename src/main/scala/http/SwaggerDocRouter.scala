ebc05d1b6c6a0b69cb446e9402468fd8110512ea
Vadim Bondarev <haghard84@gmail.com>
2016-09-19 12:35:34 +0300

    clean up SwaggerDocRouter

--- src/main/scala/http/SwaggerDocRouter.scala
+++ src/main/scala/http/SwaggerDocRouter.scala
@@ -10,7 +10,6 @@ import scala.concurrent.ExecutionContext
 //https://blog.codecentric.de/en/2016/04/swagger-akka-http/
 class SwaggerDocRouter(override val host: String, override val httpPort: Int)
                       (implicit val ec: ExecutionContext, val system: ActorSystem) extends SecuritySupport {
-  //mixin: MicroKernel ⇒
 
   //http://localhost:8001/api-docs/swagger.json
   val assets =
@@ -20,13 +19,6 @@ class SwaggerDocRouter(override val host: String, override val httpPort: Int)
 
   val route = corsHandler(new SwaggerDocService(system, s"${host}:${httpPort}").routes) ~ assets
 
-  /*abstract override def configureApi() =
-    super.configureApi() ~ Api(
-      route = Option { ec: ExecutionContext ⇒ corsHandler(new SwaggerDocService(system, s"${localAddress}:${httpPort}").routes) },
-      postAction = Option(() ⇒ system.log.info(s"★ ★ ★ Enable SwaggerDoc ★ ★ ★")),
-      urls = "")*/
-
-
-  override implicit val timeout = Timeout(1.seconds)
+  override implicit val timeout = Timeout(3.seconds)
   override protected val httpPrefixAddress: String = ""
 }
