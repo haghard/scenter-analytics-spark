e54b85a981f704fd708149e84bba3466aa5caec3
Vadim Bondarev <haghard84@gmail.com>
2016-09-30 18:57:50 +0300

    add Reb

--- src/main/scala/http/PtsLeadersRouter.scala
+++ src/main/scala/http/PtsLeadersRouter.scala
@@ -12,8 +12,6 @@ import scala.concurrent.duration._
 import scala.concurrent.{Future, ExecutionContext}
 
 object PtsLeadersRouter {
-  //type HttpArgs = JobManagerProtocol with DefaultJobArgs
-
   trait LeadersProtocol extends PlayersProtocol {
     implicit val ptsLeaderFormat = jsonFormat4(PtsLeader.apply)
     implicit val rebLeaderFormat = jsonFormat6(RebLeader.apply)
@@ -60,13 +58,13 @@ class PtsLeadersRouter(override val host: String, override val httpPort: Int,
   private val jobSupervisor = system.actorOf(SparkQuerySupervisor.props)
   override implicit val timeout = akka.util.Timeout(10.seconds)
 
-  val route = ptsLeadersRoute()
+  val route = ptsLeadersRoute() ~ rebLeadersRoute()
 
   @Path("/{stage}")
-  @ApiOperation(value = "Search stats by stage", notes = "", httpMethod = "GET")
+  @ApiOperation(value = "Search pts leaders by stage", notes = "", httpMethod = "GET")
   @ApiImplicitParams(Array(
     new ApiImplicitParam(name = "Authorization", value = "Authorization token", required = true, dataType = "string", paramType = "header"),
-    new ApiImplicitParam(name = "stage", value = "Stage of results", required = true, dataType = "string", paramType = "path"),
+    new ApiImplicitParam(name = "stage", value = "Stage of results. Examples: season-14-15,playoff-15-16", required = true, dataType = "string", paramType = "path"),
     new ApiImplicitParam(name = "depth", value = "Depth of results", required = false, dataType = "int", paramType = "query")
   ))
   @ApiResponses(Array(
@@ -87,19 +85,36 @@ class PtsLeadersRouter(override val host: String, override val httpPort: Int,
           }
         }
       }
+    }
 
-      /*~
-        (get & path(httpPrefixAddress / "reb" / Segment)) { stage ⇒
-          parameters(('depth.as[Int] ? defaultDepth)) { depth ⇒
-            withUri { url ⇒
-              requiredHttpSession(ec) { session ⇒
-                system.log.info(s"[user:${session.user}] accesses $url")
-                get(complete(searchReb(url, stage, depth)))
-              }
+
+  @Path("/{stage}")
+  @ApiOperation(value = "Search reb leaders by stage", notes = "", httpMethod = "GET")
+  @ApiImplicitParams(Array(
+    new ApiImplicitParam(name = "Authorization", value = "Authorization token", required = true, dataType = "string", paramType = "header"),
+    new ApiImplicitParam(name = "stage", value = "Stage of results. Examples: season-14-15,playoff-15-16", required = true, dataType = "string", paramType = "path"),
+    new ApiImplicitParam(name = "depth", value = "Depth of results", required = false, dataType = "int", paramType = "query")
+  ))
+  @ApiResponses(Array(
+    new ApiResponse(code = 200, message = "Ok", response = classOf[PtsLeadersView]),
+    new ApiResponse(code = 403, message = "The supplied authentication is not authorized to access this resource"),
+    new ApiResponse(code = 404, message = "Unsupported season or team"),
+    new ApiResponse(code = 500, message = "Internal server error")
+  ))
+  def rebLeadersRoute(): Route =
+    pathPrefix(pathPrefix) {
+      (get & path(httpPrefixAddress / "reb" / Segment)) { stage ⇒
+        parameters(('depth.as[Int] ? defaultDepth)) { depth ⇒
+          withUri { url ⇒
+            requiredHttpSession(ec) { session ⇒
+              system.log.info(s"[user:${session.user}] accesses $url")
+              get(complete(searchReb(url, stage, depth)))
             }
           }
-        }*/
+        }
+      }
     }
+
 
   def searchReb(url: String, stage: String, depth: Int): Future[HttpResponse] = {
     validatePeriod(stage).fold({ error => Future.successful(notFound(s"Invalid parameters: $error")) }, { _ =>
